e8f1395d6d70dd10aebff332e27af2ac4415d723
ficochiossone <ficochiossone@yahoo.com>
2018-06-04 23:14:54 -0300

    templates

--- servicios/urls.py
+++ servicios/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import include,url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.post_listado)
+    url(r'^$',views.index),
+#    url(r'^$',views.post_listado),
+    url(r'^bitacora$',views.bitacora),
+    url(r'dyh',views.dyhactual),
+
 ]
--- servicios/views.py
+++ servicios/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from acupor.models import Pak_of_inst
+
+import datetime
 
 # Create your views here
+def index(request):
+    esps_cuporizadas = Pak_of_inst.objects.all()
+    esp_list=esps_cuporizadas.order_by('institucion')
+    context = {'esp_list':esp_list}
+    return render(request,'servicios/index.html',context)
+
+
 def post_listado(request):
     return render(request,'servicios/post_listado.html',{})
+def bitacora (request):
+    return render(request,'servicios/post_listado.html',{})
+def dyhactual(request):
+    ahora = datetime.datetime.now()
+    mihtml = "<html><body> FECHA Y HORA %s .</body></html>"%ahora
+    return HttpResponse(mihtml)
     
